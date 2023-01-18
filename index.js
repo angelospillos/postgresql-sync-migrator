8266c4c817c90638ebe79a3982f170d02c2f4b04
Angelos Pillos <angelospillos@gmail.com>
2023-01-18 12:07:53 +0200

    Code improvements

--- index.js
+++ index.js
@@ -1,118 +1,94 @@
 const { exec } = require('child_process');
-const winston = require('winston');
-const cron = require('node-cron');
 const fs = require('fs');
-require('dotenv').config()
+const winston = require('winston');
+const { createLogger, format, transports } = winston;
+const { combine, timestamp, label, printf } = format;
+const myFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
+    format: combine(
+        timestamp(),
+        myFormat
+    ),
+    transports: [new transports.Console()]
+});
+
+require('dotenv').config();
 
 const sourceDbString = process.env.DATABASE_URL_SOURCE;
 const targetDbString = process.env.DATABASE_URL_TARGET;
 const backupFile = `${new Date().toISOString()}.sql`;
 
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.File({ filename: 'backup.log' }),
-        new winston.transports.Console({
-            level: 'debug',
-            format: winston.format.simple()
-        }),
-    ],
-});
-
-// Backup source database
-const backup = () => {
-    if (!fs.existsSync(backupFile)) {
-        logger.info(`Backup is being created at ${backupFile}`);
-        exec(`pg_dump --dbname=${sourceDbString} --format=c -f ${backupFile}`, (error, stdout, stderr) => {
-            if (error) {
-                logger.error(`Error creating backup: ${error}`);
+const retry = (fn, retriesLeft = 5, interval = 1000) => {
+    while (retriesLeft) {
+        try {
+            fn();
+            return;
+        } catch (error) {
+            logger.error(`Error: ${error}`);
+            retriesLeft--;
+            if (retriesLeft) {
+                logger.warn(`Retrying in ${interval}ms: ${error}`);
+                setTimeout(() => { }, interval);
+            }
+            if (retriesLeft === 0) {
+                logger.error(`Retries exhausted, giving up: ${error}`);
                 return;
             }
-            logger.info(`Backup created at ${backupFile}`);
-            createTempDb();
-        });
-    } else {
-        logger.info(`Using existing backup at ${backupFile}`);
-        createTempDb();
+        }
     }
 };
 
-// Create temporary database
-const tempDbString = `${targetDbString}_temp`;
-const tempDbUrl = new URL(tempDbString);
-const createTempDb = () => {
-    logger.info(`Temporary database ${tempDbString} is being created`);
-    exec(`createdb -h ${tempDbUrl.hostname} -p ${tempDbUrl.port} -U ${tempDbUrl.username} -T template0 ${tempDbUrl.pathname.substring(1)} -O ${tempDbUrl.username}`, (error, stdout, stderr) => {
+const createBackup = () => {
+    logger.info(`Backup is being created at ${backupFile}`);
+    exec(`pg_dump --dbname=${sourceDbString} --clean --if-exists -f ${backupFile}`, (error, stdout, stderr) => {
         if (error) {
-            logger.error(`Error creating temporary database: ${error}`);
-            return;
+            throw new Error(`Error creating backup: ${error}`);
         }
-        logger.info(`Temporary database ${tempDbString} created`);
+        logger.info(`Backup created at ${backupFile}`);
         restoreDb();
     });
 };
 
-// Restore to temporary database
 const restoreDb = () => {
-    logger.info(`Temporary database ${tempDbString} is being restored`);
-    exec(`pg_restore --dbname=${tempDbString} --format=c ${backupFile}`, (error, stdout, stderr) => {
+    logger.info(`Database ${targetDbString} is being restored`);
+    exec(`psql --dbname=${sourceDbString} -f ${backupFile}`, (error, stdout, stderr) => {
         if (error) {
-            logger.error(`Error restoring database: ${error}`);
-            return;
+            throw new Error(`Error restoring database: ${error}`);
         }
-        logger.info(`Temporary database ${tempDbString} restored`);
-        renameDb();
+        logger.info(`Database ${targetDbString} restored`);
+        removeBackup();
     });
 };
 
-// Rename temporary database to target database
-const renameDb = () => {
-    logger.info(`Temporary database ${tempDbString} is being renamed to ${targetDbString}`);
-    exec(`psql --dbname=${tempDbString} -c "ALTER DATABASE ${tempDbString} RENAME TO ${targetDbString};"`, (error, stdout, stderr) => {
-        if (error) {
-            logger.error(`Error renaming database: ${error}`);
-            return;
+const removeBackup = () => {
+    logger.info(`Backup ${backupFile} is being deleted`);
+    fs.unlink(backupFile, (err) => {
+        if (err) {
+            logger.error(`Error deleting backup file: ${err}`);
+        } else {
+            logger.info(`Backup file ${backupFile} deleted`);
         }
-        logger.info(`Temporary database ${tempDbString} renamed to ${targetDbString}`);
-        fs.unlink(backupFile, (err) => {
-            if (err) {
-                logger.error(`Error deleting backup file: ${err}`);
-            } else {
-                logger.info(`Backup file ${backupFile} deleted`);
-            }
-        });
     });
 };
 
-const retry = (fn, retriesLeft = 3, interval = 10000) => {
-    try {
-        fn();
-    } catch (error) {
-        if (retriesLeft === 0) {
-            logger.error(`Retries exhausted, giving up: ${error}`);
-            return;
-        }
-        logger.warn(`Retrying in ${interval}ms: ${error}`);
-        setTimeout(() => retry(fn, retriesLeft - 1, interval), interval);
-    };
+const run = () => {
+    if (process.env.FAILOVER_RETRIES && process.env.FAILOVER_RETRY_DELAY_MS) {
+        retry(createBackup, process.env.FAILOVER_RETRIES, process.env.FAILOVER_RETRY_DELAY_MS);
+    } else {
+        retry(createBackup);
+    }
 };
 
 cron.schedule(process.env.SCHEDULE_TIME, () => {
-    if (process.env.FAILOVER_RETRIES && process.env.FAILOVER_RETRY_DELAY_MS) {
-        retry(backup, process.env.FAILOVER_RETRIES, process.env.FAILOVER_RETRY_DELAY_MS);
-    } else {
-        retry(backup);
-    }
+    run();
 }, {
     scheduled: true,
     timezone: process.env.SCHEDULE_TIMEZONE
 });
 
 if (process.env.RUN_ON_STARTUP === 'true') {
-    if (process.env.FAILOVER_RETRIES && process.env.FAILOVER_RETRY_DELAY_MS) {
-        retry(backup, process.env.FAILOVER_RETRIES, process.env.FAILOVER_RETRY_DELAY_MS);
-    } else {
-        retry(backup);
-    }
+    run();
 }
