cefb2750fdeebdd1f09a53fa53ee82f7ad85c15a
Angelos Pillos <angelospillos@gmail.com>
2023-01-19 15:02:52 +0200

    Added endpoint for healthcheck

--- index.js
+++ index.js
@@ -93,3 +93,22 @@ cron.schedule(process.env.SCHEDULE_TIME, () => {
 if (process.env.RUN_ON_STARTUP === 'true') {
     run();
 }
+
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: process.env.PORT || 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
